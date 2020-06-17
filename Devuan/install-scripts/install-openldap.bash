2d7cf4fc5019ecd41ea30a91c63e93149a20bd46
root <root@linuxito.com>
2020-06-17 16:11:59 +0000

    updated install-openldap.bash

--- Devuan/install-scripts/install-openldap.bash
+++ Devuan/install-scripts/install-openldap.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OPENLDAP_VERSION="2.4.45"
+OPENLDAP_VERSION="2.4.50"
 tar -axf openldap-${OPENLDAP_VERSION}.tgz \
 && chown -R root:root openldap-${OPENLDAP_VERSION}/ \
 && cd openldap-${OPENLDAP_VERSION}/ \
@@ -9,7 +9,7 @@ tar -axf openldap-${OPENLDAP_VERSION}.tgz \
 export CFLAGS="-O3 -march=native -mtune=native"
 export CXXFLAGS=${CFLAGS}
 
-INSTALLDIR="/usr/local"
+INSTALLDIR="/usr/local/openldap-${OPENLDAP_VERSION}"
 
 make dist clean
 
